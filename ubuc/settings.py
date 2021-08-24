252821d182382684a436ef1e28f57447af3d4a99
Jon Massey <jon.massey@thedatalab.org>
2021-08-24 22:11:32 +0000

    feat: add bootstrap 5

--- ubuc/settings.py
+++ ubuc/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'equipmentdb'
+    'equipmentdb',
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
