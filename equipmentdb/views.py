9b31898948e3cb11dc1ec92add50ffdb575be6a6
Jon Massey <jon.massey@thedatalab.org>
2021-08-25 17:21:26 +0000

    refactor: DRY out repeated view code

--- equipmentdb/views.py
+++ equipmentdb/views.py
@@ -15,15 +15,12 @@ from django.template import loader
 class DateInput(forms.DateInput):
     input_type = "date"
 
-
 def index(request):
     template = loader.get_template('equipmentdb/index.html')
     return HttpResponse(template.render())
 
-
-# equipment types
-class EquipmentTypeListView(ListView):
-    model = EquipmentType
+#base views
+class UbucBaseListView(ListView):
     paginate_by = 20
 
     def get_context_data(self, **kwargs):
@@ -31,72 +28,19 @@ class EquipmentTypeListView(ListView):
         context["now"] = timezone.now()
         return context
 
-
-class EquipmentTypeCreateView(CreateView):
-    model = EquipmentType
-    fields = ["name"]
-    template_name_suffix = "_add_form"
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(EquipmentTypeCreateView, self).dispatch(*args, **kwargs)
-
-    def form_valid(self, form):
-        obj = form.save(commit=False)
-        obj.created_by = self.request.user
-        obj.updated_by = self.request.user
-        return super(EquipmentTypeCreateView, self).form_valid(form)
-
-
-class EquipmentTypeUpdateView(UpdateView):
-    model = EquipmentType
-    fields = ["name"]
-    template_name_suffix = "_update_form"
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(EquipmentTypeUpdateView, self).dispatch(*args, **kwargs)
-
-    def form_valid(self, form):
-        obj = form.save(commit=False)
-        obj.updated_by = self.request.user
-        return super(EquipmentTypeUpdateView, self).form_valid(form)
-
-
-class EquipmentTypeDeleteView(DeleteView):
-    model = EquipmentType
-    success_url = reverse_lazy("equipment-type-list")
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(EquipmentTypeDeleteView, self).dispatch(*args, **kwargs)
-
-
-# equipment items
-class EquipmentListView(ListView):
-    model = Equipment
-    paginate_by = 20
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["now"] = timezone.now()
-        return context
-
-
-class EquipmentCreateView(CreateView):
-    model = Equipment
+class UbucBaseCreateView(CreateView):
     fields = "__all__"
     template_name_suffix = "_add_form"
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
-        return super(EquipmentCreateView, self).dispatch(*args, **kwargs)
+        return super(UbucBaseCreateView, self).dispatch(*args, **kwargs)
 
     def form_valid(self, form):
         obj = form.save(commit=False)
         obj.created_by = self.request.user
         obj.updated_by = self.request.user
-        return super(EquipmentCreateView, self).form_valid(form)
+        return super(UbucBaseCreateView, self).form_valid(form)
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
@@ -108,22 +52,59 @@ class EquipmentCreateView(CreateView):
                 form.fields[f.name].widget = DateInput()
         return form
 
-
-class EquipmentUpdateView(UpdateView):
-    model = Equipment
+class UbucBaseUpdateView(UpdateView):
+    model = EquipmentType
     fields = "__all__"
     template_name_suffix = "_update_form"
 
-    NoteFormSet = inlineformset_factory(Equipment,EquipmentNote,fields=('notes',),extra=1)
-    
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
-        return super(EquipmentUpdateView, self).dispatch(*args, **kwargs)
+        return super(UbucBaseUpdateView, self).dispatch(*args, **kwargs)
 
     def form_valid(self, form):
         obj = form.save(commit=False)
         obj.updated_by = self.request.user
-        return super(EquipmentUpdateView, self).form_valid(form)
+        return super(UbucBaseUpdateView, self).form_valid(form)
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+
+        for f in self.model._meta.get_fields(
+            include_parents=False, include_hidden=False
+        ):
+            if isinstance(f, DateField) and f.name in form.fields:
+                form.fields[f.name].widget = DateInput()
+        return form
+
+class UbucBaseDeleteView(DeleteView):
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(UbucBaseDeleteView, self).dispatch(*args, **kwargs)
+
+# equipment types
+class EquipmentTypeListView(UbucBaseListView):
+    model = EquipmentType
+
+class EquipmentTypeCreateView(UbucBaseCreateView):
+    model = EquipmentType
+
+class EquipmentTypeUpdateView(UbucBaseUpdateView):
+    model = EquipmentType
+
+class EquipmentTypeDeleteView(UbucBaseDeleteView):
+    model = EquipmentType
+    success_url = reverse_lazy("equipment-type-list")
+
+# equipment items
+class EquipmentListView(UbucBaseListView):
+    model = Equipment
+
+class EquipmentCreateView(UbucBaseCreateView):
+    model = Equipment
+
+class EquipmentUpdateView(UbucBaseUpdateView):
+    model = Equipment
+    NoteFormSet = inlineformset_factory(Equipment,EquipmentNote,fields=('notes',),extra=1)   
 
     def get_context_data(self, **kwargs):
         ctx =  super().get_context_data(**kwargs)
@@ -132,71 +113,20 @@ class EquipmentUpdateView(UpdateView):
         ctx['notes'] = notes
         return ctx
 
-    def get_form(self, form_class=None):
-        
-        form = super().get_form(form_class)
-
-        for f in self.model._meta.get_fields(
-            include_parents=False, include_hidden=False
-        ):
-            if isinstance(f, DateField) and f.name in form.fields:
-                form.fields[f.name].widget = DateInput()
-        return form
-
-
-class EquipmentDeleteView(DeleteView):
+class EquipmentDeleteView(UbucBaseDeleteView):
     model = Equipment
     success_url = reverse_lazy("equipment-list")
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(EquipmentDeleteView, self).dispatch(*args, **kwargs)
-
-#EquipmentNote
-
-class EquipmentNoteListView(ListView):
+# Equipment Notes
+class EquipmentNoteListView(UbucBaseListView):
     model = EquipmentNote
-    paginate_by = 20
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["now"] = timezone.now()
-        return context
-
-
-class EquipmentNoteCreateView(CreateView):
+class EquipmentNoteCreateView(UbucBaseCreateView):
     model = EquipmentNote
-    fields = "__all__"
-    template_name_suffix = "_add_form"
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(EquipmentNoteCreateView, self).dispatch(*args, **kwargs)
-
-    def form_valid(self, form):
-        obj = form.save(commit=False)
-        obj.created_by = self.request.user
-        obj.updated_by = self.request.user
-        return super(EquipmentNoteCreateView, self).form_valid(form)
-
-class EquipmentNoteUpdateView(UpdateView):
+class EquipmentNoteUpdateView(UbucBaseUpdateView):
     model = EquipmentNote
-    fields = "__all__"
-    template_name_suffix = "_update_form"
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(EquipmentNoteUpdateView, self).dispatch(*args, **kwargs)
-
-    def form_valid(self, form):
-        obj = form.save(commit=False)
-        obj.updated_by = self.request.user
-        return super(EquipmentNoteUpdateView, self).form_valid(form)
-
-class EquipmentNoteDeleteView(DeleteView):
+class EquipmentNoteDeleteView(UbucBaseDeleteView):
     model = EquipmentNote
     success_url = reverse_lazy("equipment-list")
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(EquipmentNoteDeleteView, self).dispatch(*args, **kwargs)
