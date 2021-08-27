ea8263218117dbc9fad996872d26f4b948dba1d2
Jon Massey <jon.massey@thedatalab.org>
2021-08-27 17:06:23 +0000

    refactor: declutter views.py

---
+++ equipmentdb/view_base.py
@@ -0,0 +1,77 @@
+from django import forms
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.db.models.fields import DateField
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+# base views
+class UbucBaseListView(ListView):
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
+
+
+class UbucBaseCreateView(CreateView):
+    fields = "__all__"
+    template_name_suffix = "_add_form"
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(UbucBaseCreateView, self).dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.created_by = self.request.user
+        obj.updated_by = self.request.user
+        return super(UbucBaseCreateView, self).form_valid(form)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+
+        for f in self.model._meta.get_fields(
+            include_parents=False, include_hidden=False
+        ):
+            if isinstance(f, DateField) and f.name in form.fields:
+                form.fields[f.name].widget = DateInput()
+        return form
+
+
+class UbucBaseUpdateView(UpdateView):
+    fields = "__all__"
+    template_name_suffix = "_update_form"
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(UbucBaseUpdateView, self).dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.updated_by = self.request.user
+        return super(UbucBaseUpdateView, self).form_valid(form)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+
+        for f in self.model._meta.get_fields(
+            include_parents=False, include_hidden=False
+        ):
+            if isinstance(f, DateField) and f.name in form.fields:
+                form.fields[f.name].widget = DateInput()
+        return form
+
+
+class UbucBaseDeleteView(DeleteView):
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        self.success_url = self.get_object().get_absolute_url()
+        return super(UbucBaseDeleteView, self).dispatch(*args, **kwargs)
--- equipmentdb/views.py
+++ equipmentdb/views.py
@@ -1,93 +1,20 @@
-from typing import Any
-from django.db.models.fields import DateField
 from equipmentdb.models import EquipmentType
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic.list import ListView
 from equipmentdb.models import *
-from django.http import HttpResponse
-from django.urls import reverse_lazy
-from django.utils import timezone
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from equipmentdb.view_base import (
+    UbucBaseCreateView,
+    UbucBaseListView,
+    UbucBaseDeleteView,
+    UbucBaseUpdateView,
+)
 from django import forms
 from django.forms import inlineformset_factory
+from django.http import HttpResponse
 from django.template import loader
-
-
-class DateInput(forms.DateInput):
-    input_type = "date"
 
 
 def index(request):
     template = loader.get_template("equipmentdb/index.html")
     return HttpResponse(template.render())
-
-
-# base views
-class UbucBaseListView(ListView):
-    paginate_by = 20
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["now"] = timezone.now()
-        return context
-
-
-class UbucBaseCreateView(CreateView):
-    fields = "__all__"
-    template_name_suffix = "_add_form"
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(UbucBaseCreateView, self).dispatch(*args, **kwargs)
-
-    def form_valid(self, form):
-        obj = form.save(commit=False)
-        obj.created_by = self.request.user
-        obj.updated_by = self.request.user
-        return super(UbucBaseCreateView, self).form_valid(form)
-
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class)
-
-        for f in self.model._meta.get_fields(
-            include_parents=False, include_hidden=False
-        ):
-            if isinstance(f, DateField) and f.name in form.fields:
-                form.fields[f.name].widget = DateInput()
-        return form
-
-
-class UbucBaseUpdateView(UpdateView):
-    model = EquipmentType
-    fields = "__all__"
-    template_name_suffix = "_update_form"
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(UbucBaseUpdateView, self).dispatch(*args, **kwargs)
-
-    def form_valid(self, form):
-        obj = form.save(commit=False)
-        obj.updated_by = self.request.user
-        return super(UbucBaseUpdateView, self).form_valid(form)
-
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class)
-
-        for f in self.model._meta.get_fields(
-            include_parents=False, include_hidden=False
-        ):
-            if isinstance(f, DateField) and f.name in form.fields:
-                form.fields[f.name].widget = DateInput()
-        return form
-
-
-class UbucBaseDeleteView(DeleteView):
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        self.success_url = self.get_object().get_absolute_url()
-        return super(UbucBaseDeleteView, self).dispatch(*args, **kwargs)
 
 
 # equipment types
@@ -191,37 +118,48 @@ class EquipmentNoteDeleteView(UbucBaseDeleteView):
 class EquipmentTypeServiceScheduleListView(UbucBaseListView):
     model = EquipmentTypeServiceSchedule
 
+
 class EquipmentTypeServiceScheduleCreateView(UbucBaseCreateView):
     model = EquipmentTypeServiceSchedule
+
 
 class EquipmentTypeServiceScheduleUpdateView(UbucBaseUpdateView):
     model = EquipmentTypeServiceSchedule
 
+
 class EquipmentTypeServiceScheduleDeleteView(UbucBaseDeleteView):
     model = EquipmentTypeServiceSchedule
+
 
 # Test Schedules
 class EquipmentTypeTestScheduleListView(UbucBaseListView):
     model = EquipmentTypeTestSchedule
 
+
 class EquipmentTypeTestScheduleCreateView(UbucBaseCreateView):
     model = EquipmentTypeTestSchedule
+
 
 class EquipmentTypeTestScheduleUpdateView(UbucBaseUpdateView):
     model = EquipmentTypeTestSchedule
 
+
 class EquipmentTypeTestScheduleDeleteView(UbucBaseDeleteView):
     model = EquipmentTypeTestSchedule
+
 
 # Test Types
 class TestTypeListView(UbucBaseListView):
     model = TestType
 
+
 class TestTypeCreateView(UbucBaseCreateView):
     model = TestType
 
+
 class TestTypeUpdateView(UbucBaseUpdateView):
     model = TestType
+
 
 class TestTypeDeleteView(UbucBaseDeleteView):
     model = TestType
